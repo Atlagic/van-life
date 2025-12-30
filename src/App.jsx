5f927c26e94bb29e8b6478c87a05fc2d5fd1dc08
Aleksandar Atlagic <atlagic.aleksandar@gmail.com>
2025-12-30 18:40:55 +0100

    Fetching data from fake local server "miragejs" and displaying info from the api

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,20 @@ import About from "./pages/About.jsx";
 import Home from "./pages/Home.jsx";
 import Vans from "./pages/Vans.jsx";
 import Header from "./components/Header.jsx";
+import "../server.js";
 
 function App() {
 
-  return (
+    return (
       <BrowserRouter>
           <Header />
           <Routes>
               <Route path="/" element={<Home/>} />
               <Route path="/about" element={<About/>} />
-              <Route path="/vans" element={<Vans/>} />
+              <Route path="/vans" element={<Vans />} />
           </Routes>
       </BrowserRouter>
-  )
+    )
 }
 
 export default App
