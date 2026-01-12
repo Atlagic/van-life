2d832894e912bfaad6d313a4a8a54e130d295e4a
Aleksandar Atlagic <atlagic.aleksandar@gmail.com>
2026-01-12 19:25:58 +0100

    Using RouterProvider instead of BrowserRouter ( tackling loaders )

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,10 @@
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
+import {
+    RouterProvider,
+    createBrowserRouter,
+    createRoutesFromElements,
+    Route,
+    Link
+} from "react-router-dom"
 import About from "./pages/About.jsx";
 import Home from "./pages/Home.jsx";
 import Login from "./pages/Login.jsx";
@@ -19,40 +25,35 @@ import NotFound from "./pages/NotFound.jsx";
 
 import "../server.js";
 
+const router = createBrowserRouter(createRoutesFromElements(
+    <Route path="/" element={<Layout />}>
+        <Route index element={<Home/>} />
+        <Route path="about" element={<About/>} />
+        <Route path="vans" element={<Vans />} />
+        <Route path="vans/:id" element={<VanDetail />} />
+        <Route path="login" element={<Login />} />
 
+        <Route element={<AuthRequired />} >
+            <Route path="host" element={<HostLayout />}>
+                <Route index element={<Dashboard />} />
+                <Route path="vans" element={<HostVans />} />
+                <Route path="vans/:id" element={<HostVanDetail />}>
+                    <Route index element={<HostVanInfo />} />
+                    <Route path="pricing" element={<HostVanPricing />} />
+                    <Route path="photos" element={<HostVanPhotos />} />
+                </Route>
 
+                <Route path="income" element={<Income />} />
+                <Route path="reviews" element={<Reviews />} />
+            </Route>
+        </Route>
 
+        <Route path="*" element={<NotFound/>} />
+    </Route>
+))
 function App() {
-
     return (
-      <BrowserRouter>
-          <Routes>
-              <Route path="/" element={<Layout />}>
-                  <Route index element={<Home/>} />
-                  <Route path="about" element={<About/>} />
-                  <Route path="vans" element={<Vans />} />
-                  <Route path="vans/:id" element={<VanDetail />} />
-                  <Route path="login" element={<Login />} />
-
-                  <Route element={<AuthRequired />} >
-                      <Route path="host" element={<HostLayout />}>
-                          <Route index element={<Dashboard />} />
-                          <Route path="vans" element={<HostVans />} />
-                          <Route path="vans/:id" element={<HostVanDetail />}>
-                              <Route index element={<HostVanInfo />} />
-                              <Route path="pricing" element={<HostVanPricing />} />
-                              <Route path="photos" element={<HostVanPhotos />} />
-                          </Route>
-
-                          <Route path="income" element={<Income />} />
-                          <Route path="reviews" element={<Reviews />} />
-                      </Route>
-                  </Route>
-
-                  <Route path="*" element={<NotFound/>} />
-              </Route>
-          </Routes>
-      </BrowserRouter>
+      <RouterProvider router={router} />
     )
 }
 
